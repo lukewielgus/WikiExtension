a1e8e7da205edcf99c670f9562d54a653c702252
Brian Faure <bfaure23@gmail.com>
2017-03-30 15:06:40 -0400

    only highlights now when clicked

--- js/background.js
+++ js/background.js
@@ -1,3 +1,4 @@
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.executeScript(null, {file: "js/content.js"});
+    //chrome.tabs.executeScript(null, {file: "js/popup.js"});
 });
