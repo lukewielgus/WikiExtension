f20a5c1e25d452bddd9efe8474e7f416cafd11d6
Brian Faure <bfaure23@gmail.com>
2017-03-30 19:28:01 -0400

    improved code readability

--- popup/popup.js
+++ popup/popup.js
@@ -14,10 +14,12 @@ function process_url(url)
 
 	console.log(document)	
 
+	// create iFrame element to insert later
 	var iFrame = document.createElement("iframe");
 	iFrame.src = chrome.extension.getURL("popup/popup_box.htm");
 	iFrame.style = "border:1px solid grey;";
 
+	// if this is the home page, resize to a banner
 	if (tablink=="https://www.wikipedia.org/" || tablink=="https://www.wikipedia.org")
 	{
 		iFrame.width = (parseInt(document.body.clientWidth)).toString();
@@ -26,11 +28,20 @@ function process_url(url)
 		document.body.insertBefore(iFrame, document.body.firstChild);
 		return;
 	}
+
+	// if this is the main page, skip
 	if (tablink=="https://en.wikipedia.org/wiki/Main_Page")
 	{
 		return;
 	}
 
+	// if this is a talk page, skip
+	if (tablink.indexOf("Talk:")!=-1)
+	{
+		return;
+	}
+
+	// if we get here, we know its an article page
 	iFrame.width = "550";
 	iFrame.height = "300";
 	iFrame.align = "right";
@@ -43,9 +54,6 @@ function process_url(url)
 	var insert_spot = content_handle.children[4];
 	content_handle.insertBefore(iFrame,insert_spot);
 
-
-	//document.body.style.webkitTransform = "translateY(1000px)";
-	//document.documentElement.appendChild(iFrame);
 }
 
 get_url(process_url);
--- popup/popup_generator.js
+++ popup/popup_generator.js
@@ -6,17 +6,21 @@ function jQueryMain () {
 	{
 		var tablink = tab.url;
 
+		// if this will be a banner, don't add content
 		if (tablink=="https://www.wikipedia.org" || tablink=="https://www.wikipedia.org/")
 		{
 			return;
 		}
 
+		// get the article name
 		var article = tablink.split("/wiki/")[1];
-
 		var article_line = "Article: "+article;
 
+		// add the article title
 		$("body").append("<p>"+article_line+"</p>");
-	});
 
-    //$("body").append ('<p>Added by jQuery</p>');
+		// here is where we would make calls to server to get article details...
+		// quality = server.get_quality(article)
+
+	});
 }
