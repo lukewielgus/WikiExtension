31b56dfd9a8ca97ec665458d2e90e96276de74a5
Brian Faure <bfaure23@gmail.com>
2017-03-30 19:13:31 -0400

    resizing of box if on an article page

--- popup/popup.js
+++ popup/popup.js
@@ -12,27 +12,40 @@ function process_url(url)
 {
 	var tablink = url;
 
+	//console.log(document)	
+
 	var iFrame = document.createElement("iframe");
 	iFrame.src = chrome.extension.getURL("popup/popup_box.htm");
-	iFrame.style = "border:3px solid grey;";
+	iFrame.style = "border:1px solid grey;";
 
 	if (tablink=="https://www.wikipedia.org/")
 	{
 		iFrame.width = (parseInt(document.body.clientWidth)-25).toString();
 		iFrame.height = "250";
 		iFrame.align = "center";
-
-
+		document.body.insertBefore(iFrame, document.body.firstChild);
+		return;
 	}
-	else
+	if (tablink=="https://en.wikipedia.org/wiki/Main_Page")
 	{
-		iFrame.width = "250";
-		iFrame.height = "250";
-		iFrame.align = "right";
-
+		return;
 	}
 
-	document.body.insertBefore(iFrame, document.body.firstChild);
+	iFrame.width = "500";
+	iFrame.height = "300";
+	iFrame.align = "right";
+	iFrame.hspace = "100";
+
+	var content_handle = document.getElementById("content");
+	
+	console.log(content_handle);
+
+	var insert_spot = content_handle.children[4];
+	content_handle.insertBefore(iFrame,insert_spot);
+
+
+	//document.body.style.webkitTransform = "translateY(1000px)";
+	//document.documentElement.appendChild(iFrame);
 }
 
 get_url(process_url);
