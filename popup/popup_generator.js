5dcaa27aac9ce275424fdbe7965a059044c78473
Brian Faure <bfaure23@gmail.com>
2017-04-04 15:34:42 -0400

    popularity plot now broken down by agent

--- popup/popup_generator.js
+++ popup/popup_generator.js
@@ -21,6 +21,19 @@ function get_pageviews(article_name,increment,start,end)
 	return data;
 }
 
+// article name: (with underscores)
+// increment: [daily,monthly]
+// start: YYYYMMDD format
+// end: YYYYMMDD format
+function get_pageviews_agent(article_name,increment,start,end,agent)
+{
+	var url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/";
+	url += "en.wikipedia.org/all-access/"+agent+"/"+article_name+"/";
+	url += increment+"/"+start+"/"+end;
+	var data = get_http_xml(url);
+	return data;
+}
+
 function get_daily_views(article_name,year)
 {
 
@@ -89,12 +102,12 @@ function make_view_plot(article_name)
 {
 
 	var interval = "daily";
-
 	var start_date = "20150101";
 
 	var d1 = new Date();
 	var current_year = d1.getFullYear();
 	var current_month = d1.getMonth()+1;
+
 	if (current_month<10)
 	{
 		current_month = "0"+String(current_month);
@@ -106,33 +119,109 @@ function make_view_plot(article_name)
 	}
 	var end_date = String(current_year)+String(current_month)+String(current_day);
 
-	var data = get_pageviews(article_name,interval,start_date,end_date);
-	var obj = JSON.parse(data);
+	//$("body").append("<p> start_date:"+start_date+", end_date:"+end_date+"</p>");
+
+	var human_traffic = get_pageviews_agent(article_name,interval,start_date,end_date,"user");
+	var all_traffic = get_pageviews(article_name,interval,start_date,end_date);
+
+	var human_traffic_obj = JSON.parse(human_traffic);
+	var all_traffic_obj = JSON.parse(all_traffic);
+
+	//$("body").append("<p>here</p>");
+
+	//var data = get_pageviews(article_name,interval,start_date,end_date);
+	//var obj = JSON.parse(data);
 	
-	var view_data = 
+	var all_traffic_data = 
 	{
 		x: [],
 		y: []
 	};
 
-	var max_views = -1;
-
-	for (var i=0; i<obj.items.length; i++)
+	var human_traffic_data = 
 	{
-		view_data.y.push(obj.items[i].views);
-		view_data.x.push(i);
+		x: [],
+		y: []
+	};
 
-		if (obj.items[i].views>max_views)
+	var bot_traffic_data = 
+	{
+		x: [],
+		y: []
+	};
+
+	var human_start_index = 0;
+	var all_start_index = 0;
+	var end_index = 0;
+
+	if (human_traffic_obj.items.length<all_traffic_obj.items.length)
+	{
+		all_start_index = all_traffic_obj.items.length - human_traffic_obj.items.length;
+		end_index = human_traffic_obj.items.length;
+	}
+
+	else
+	{
+		if (all_traffic_obj.items.length<human_traffic_obj.items.length)
 		{
-			max_views = obj.items[i].views+5;
+			human_start_index = human_traffic_obj.items.length - all_traffic_obj.items.length;
+			end_index = all_traffic_obj.items.length;
+		}
+		else
+		{
+			end_index = all_traffic_obj.items.length;
 		}
 	}
+
+	var max_views = -1;
+	for (var i=0; i<end_index; i++)
+	{
+		var all_item = all_traffic_obj.items[i+all_start_index].views;
+		var human_item = human_traffic_obj.items[i+human_start_index].views;
+
+		all_traffic_data.y.push(all_item);
+		human_traffic_data.y.push(human_item);
+		bot_traffic_data.y.push(all_item-human_item);
+
+		all_traffic_data.x.push(i);
+		human_traffic_data.x.push(i);
+		bot_traffic_data.x.push(i);
+
+		if (all_item>max_views)
+		{
+			max_views = all_item+5;
+		}
+	}
+
+	var all_trace = 
+	{
+		x: all_traffic_data.x,
+		y: all_traffic_data.y,
+		name: "All Traffic",
+		type: 'scatter'
+	};
+
+	var human_trace = 
+	{
+		x: human_traffic_data.x,
+		y: human_traffic_data.y,
+		name: "Human Traffic",
+		type: 'scatter'
+	};
+
+	var bot_trace = 
+	{
+		x: bot_traffic_data.x,
+		y: bot_traffic_data.y,
+		name: "Bot Traffic",
+		type: 'scatter'
+	};
 
 	var layout = {
 		
 		xaxis: 
 		{
-			//range: [0,view_data.x.length]
+			range: [0,all_trace.x.length]
 
 		},
 
@@ -140,35 +229,36 @@ function make_view_plot(article_name)
 		{
 			range: [0,max_views]
 		},
-
+		
 		margin:
 		{
 			t: 15,
 			r: 0,
 			l: 35,
 			b: 15
+		},
+
+		showlegend: true,
+
+		legend: 
+		{
+			x: 0,
+			y: 1
 		}
 
 	};
 
-	$("body").append("<div id=\"plot\" style=\"width:240px;height:100px;\"></div>")
+	$("body").append("<div id=\"plot\" style=\"width:240px;height:150px;\"></div>")
+
 	var plot_spot = document.getElementById('plot');
+	var data = [all_trace,bot_trace,human_trace];
 
-	var view_trace = {
-		x: view_data.x,
-		y: view_data.y,
-		type: 'scatter'
-	};
-
-	var data = [view_trace];
-
-	Plotly.plot(
+	Plotly.plot
+	(
 		plot_spot,
 		data,
 		layout
-		);
-
-	return view_data;
+	);
 }
 
 
