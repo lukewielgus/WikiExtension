7e5941214dfa561c0c7fd18c71641ae28a68a1a9
bfaure <bfaure23@gmail.com>
2017-04-26 09:23:51 -0400

    minor change

--- js/highlighter.js
+++ js/highlighter.js
@@ -32,7 +32,7 @@ function create_mapping_dict()
 		{
 			var line_word = String(line_items[0]).toLowerCase(); // word of current line
 			var line_cat = line_items[1].split("\r").join(""); // category for current line
-			if (line_cat=="war" || line_cat=="populated_areas" || line_cat=="music"){  continue;  }
+			//if (line_cat=="war" || line_cat=="populated_areas" || line_cat=="music"){  continue;  }
 			var cat_idx = possible_cats.indexOf(line_cat); // index of category in 'possible_cats' and 'all_colors'
 			mapping_dict[String(line_items[0]).toLowerCase()]=cat_idx; // register the category
 		}
